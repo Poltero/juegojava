ed0fb23d5c6daf881f8f40429fadcf1273ac61b0
Dasten <carlosbceniza@gmail.com>
2013-10-17 01:37:40 +0200

    Cambios en UIManager

---
+++ src/es/juegojava/ui/Salida.java
@@ -0,0 +1,11 @@
+package es.juegojava.ui;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+public class Salida {
+
+	//to-do
+}
--- src/es/juegojava/ui/UIManager.java
+++ src/es/juegojava/ui/UIManager.java
@@ -3,10 +3,56 @@
  */
 package es.juegojava.ui;
 
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+
+import es.juegojava.common.Element;
+import es.juegojava.mapa.Item;
+import es.juegojava.mapa.Room;
+
 /**
  * @author pablo.fernandez
  *
  */
+
+//To-do Excepcion
 public class UIManager {
 
+	public void descripcion(Element elementoADescribir){
+		
+	}
+	
+	public void describirHabitacion(Room habitacionADescribir){
+				
+	}
+	
+	public void describirItem(Item itemADescribir){
+		
+	}
+	
+	public static void imprimirPorPantalla(String salida){
+		PrintWriter pw = new PrintWriter(System.out);
+		pw.println(salida);
+		pw.flush();
+		pw.close();		
+	}
+	
+	public int leerNumeroTeclado(){
+		Scanner sc = new Scanner(System.in);
+		int salida = sc.nextInt();
+		return salida;
+	}
+	
+	public String leerCadenaTeclado(){
+		Scanner sc = new Scanner(System.in);
+		String salida = sc.nextLine();
+		return salida;
+	}
+	
+	public String leerFichero(){
+		String datos = "";
+		//To-do
+		return datos;
+	}
 }
