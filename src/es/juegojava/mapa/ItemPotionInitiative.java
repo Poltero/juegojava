4cef7b66082de6df747e7bceb611187a77de617f
Dasten <carlosbceniza@gmail.com>
2013-10-24 15:04:19 +0200

    Añadida pocion Iniciativa y modificado el NPC

---
+++ src/es/juegojava/mapa/ItemPotionInitiative.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package es.juegojava.mapa;
+
+import es.juegojava.common.ItemsType;
+import es.juegojava.logics.Usable;
+import es.juegojava.players.Player;
+
+
+public class ItemPotionInitiative extends Item implements Usable {
+	
+	private int initiativePoints;
+	
+	ItemPotionInitiative(String nombre, int id, int initiativePoints) {
+		super(nombre, id, ItemsType.POCION);
+		this.initiativePoints = initiativePoints;
+	}
+
+	@Override
+	public void use(Player player) {
+		player.setAttack(player.getAttack() + initiativePoints);
+	}
+}
