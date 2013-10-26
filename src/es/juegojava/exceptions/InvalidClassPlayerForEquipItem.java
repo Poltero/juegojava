b12fe2dcfe157036d51b595efb7654f637e1218c
Pablo Fernandez <poltero@ovi.com>
2013-10-26 03:57:19 +0200

    Ya se pueden aquipar armas y armaduras

---
+++ src/es/juegojava/exceptions/InvalidClassPlayerForEquipItem.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package es.juegojava.exceptions;
+
+/**
+ * @author pfranco
+ *
+ */
+public class InvalidClassPlayerForEquipItem extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public InvalidClassPlayerForEquipItem(String message) {
+		super(message);
+	}
+	
+}
--- src/es/juegojava/players/Player.java
+++ src/es/juegojava/players/Player.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import es.juegojava.common.ClassType;
 import es.juegojava.common.Raza;
+import es.juegojava.exceptions.InvalidClassPlayerForEquipItem;
 import es.juegojava.mapa.Item;
 import es.juegojava.mapa.ItemArma;
 import es.juegojava.mapa.ItemArmadura;
@@ -37,6 +38,22 @@ public class Player extends PjWithClass
 
 	public List<Item> getInventario() {
 		return inventario;
+	}
+	
+	public void equipItem(int index) throws InvalidClassPlayerForEquipItem {
+		Item itemToEquip = inventario.get(index);
+		
+		if(itemToEquip instanceof ItemArma) {
+			ItemArma weapon = (ItemArma)itemToEquip;
+			
+			if(weapon.getWeaponClass() == this.getClassPj()) {
+				this.currentWeapon = weapon; //fallo en diagrama de clases
+			} else {
+				throw new InvalidClassPlayerForEquipItem("[No se pudo equipar] La clase del player es incomplatible con la del item. Require ser '" + weapon.getWeaponClass() + "'");
+			}
+		} else if(itemToEquip instanceof ItemArmadura) {
+			this.currentArmor = (ItemArmadura)itemToEquip;
+		}
 	}
 
 }
